eab8a30dec0616215498effb916f48378b3e2276
kampxrapan039 <kampeerapan0803@gmail.com>
2024-12-17 10:44:11 +0700

    Ex1

---
+++ ex2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- เพิ่ม fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
+    <style>
+        body{
+            font-family: "Itim", cursive;
+            font-weight: 400;
+            font-style: normal;
+            
+}
+
+        }
+    </style>
+    <!-- สิ้นสุด fonts -->
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ตัวอย่าง 1</title>
+</head>
+<body>
+    <h1> โปรแกรม ภาษา HTML </h1>
+    664485039 นางสาวคัมภีรพรรณ ศุขบุญ <br>
+    หมู่่เรียน 66/96
+    
+</body>
+</html>
